a5ca46773008a5b624a946be1dc90b023d884640
lfuhr <ludwig.fuhr@yahoo.de>
2022-02-23 08:34:43 +0100

    update url

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 script = document.createElement("script");
-script.src = "https://i.lfuhr.de/content.js";
+script.src = "https://cdn.jsdelivr.net/gh/lfuhr/chrome-codemirror-hackmd/remote.js";
 document.head.appendChild(script)
